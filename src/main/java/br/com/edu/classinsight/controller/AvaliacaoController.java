7955844e2a7fee11458da02925533eb71a993cbe
EliBrito <elileite9@gmail.com>
2026-01-03 23:06:51 -0300

    Ajustes nas configurações do Docker e adição de dependencia do postgreSQL.

--- src/main/java/br/com/edu/classinsight/controller/AvaliacaoController.java
+++ src/main/java/br/com/edu/classinsight/controller/AvaliacaoController.java
@@ -2,13 +2,19 @@ package br.com.edu.classinsight.controller;
 
 import br.com.edu.classinsight.dto.FeedbackDTO;
 import br.com.edu.classinsight.service.FeedbackService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/avaliacao")
 public class AvaliacaoController {
@@ -20,8 +26,15 @@ public class AvaliacaoController {
     }
 
     @PostMapping
-    public ResponseEntity<Void> criarAvaliacao(@RequestBody FeedbackDTO feedback){
+    public ResponseEntity<Void> criarAvaliacao(@Valid @RequestBody FeedbackDTO feedback){
+        log.info("Recebendo nova avaliacao: descricao='{}' nota={}", feedback.descricao(), feedback.nota());
         feedbackService.criarAvaliacao(feedback);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+
+    @GetMapping("/health")
+    public ResponseEntity<String> health(){
+        return ResponseEntity.ok("ok");
+    }
+
 }
--- src/main/java/br/com/edu/classinsight/service/FeedbackService.java
+++ src/main/java/br/com/edu/classinsight/service/FeedbackService.java
@@ -4,10 +4,11 @@ import br.com.edu.classinsight.dto.FeedbackDTO;
 import br.com.edu.classinsight.entity.Feedback;
 import br.com.edu.classinsight.repository.FeedbackRepository;
 import br.com.edu.classinsight.enums.Urgencia;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-
+@Slf4j
 @Service
 public class FeedbackService {
 
@@ -18,7 +19,7 @@ public class FeedbackService {
     }
 
     public void criarAvaliacao(FeedbackDTO dto){
-
+    log.info("Salvando nova avaliacao: descricao='{}' nota={}", dto.descricao(), dto.nota());
         Feedback feedback = new Feedback();
         feedback.setDescricao(dto.descricao());
         feedback.setNota(dto.nota());
@@ -26,6 +27,7 @@ public class FeedbackService {
         feedback.setCriadoEm(LocalDateTime.now());
 
         feedbackRepository.save(feedback);
+        log.info("Avaliacao salva com sucesso: id={}", feedback.getId());
     }
 
 }
